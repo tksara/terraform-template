6c4e119965e0c96d305d6ac14c5320ad84b454ed
Stoeva <stozh01@ca.com>
2018-11-29 00:32:20 +0100

    folder upload

---
+++ jeny_aws/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+	region     = "us-east-2"
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_instance" "cda_instance" {
+	ami                    = "${var.aws_ami}"
+	instance_type          = "t2.micro"
+	vpc_security_group_ids = ["${var.aws_security_group_id}"]
+//	vpc_security_group_ids = "alabala"
+//	key_name	       = "${var.aws_key_name}"
+
+}	
---
+++ jeny_aws/vars_aws.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {
+  default = "alabala"
+}
+
+variable "aws_secret_key" {
+  default = "alabala"
+}
+
+variable "aws_key_name" {
+	default = "key1"
+}
+
+variable "private_key_file" {
+	default = "./key1.pem"
+}
+variable "aws_ami" {
+	default = "ami-731c2016"
+}
+
+variable "aws_security_group_id" {
+	default = "alabala"
+}
