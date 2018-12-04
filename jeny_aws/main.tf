053b8c1f52b7deade38cf841108d906816351a70
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2018-12-04 16:11:19 +0100

    Update main.tf

--- jeny_aws/main.tf
+++ jeny_aws/main.tf
@@ -14,8 +14,16 @@ resource "aws_instance" "cda_instance" {
 	user_data = <<HEREDOC
 		#!/bin/bash
 		yum update -y
-		amazon-linux-extras install docker
+		yum install -y docker
 		service docker start
+		curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+		chmod +x /usr/local/bin/docker-compose		
+		sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+		docker-compose --version
+		yum install -y git
 		usermod -aG docker ec2-user
+		git clone git://github.com/Runscope/requestbin.git
+		docker-compose build
+		docker-compose up -d
 	HEREDOC
 }	
