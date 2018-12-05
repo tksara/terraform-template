9f819fc5402fa4c791e648a72dc19aee9ac5f216
Stoeva <stozh01@ca.com>
2018-12-05 10:08:08 +0100

    add requestbin

---
+++ aws/requestbin/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+	region     = "us-east-2"
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_instance" "cda_instance" {
+	ami                    = "${var.aws_ami}"
+	instance_type          = "t2.micro"
+	vpc_security_group_ids = ["${var.aws_security_group_id}"]
+//	vpc_security_group_ids = "alabala"
+//	key_name	       = "${var.aws_key_name}"
+	key_name	= "jeny-key"
+	user_data = <<HEREDOC
+		#!/bin/bash
+		yum update -y
+		yum install -y docker
+		service docker start
+		usermod -aG docker ec2-user
+		curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+		chmod +x /usr/local/bin/docker-compose		
+		sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+		docker-compose --version
+		yum install -y git
+		mkdir  /tmp/jenya
+                cd /tmp/jenya
+		git clone git://github.com/jenyss/requestbin.git
+		cd requestbin
+		docker-compose build
+		docker-compose up -d
+	HEREDOC
+}	
---
+++ aws/requestbin/vars_aws.tf
@@ -0,0 +1,22 @@
+variable "aws_access_key" {
+  default = "alabala"
+}
+
+variable "aws_secret_key" {
+  default = "alabala"
+}
+
+variable "aws_key_name" {
+	default = "key1"
+}
+
+variable "private_key_file" {
+	default = "./key1.pem"
+}
+variable "aws_ami" {
+	default = "ami-0cd3dfa4e37921605"	
+}
+
+variable "aws_security_group_id" {
+	default = "alabala"
+}
