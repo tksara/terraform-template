fee739839a0920d871114ba900418582ad6f89e8
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2018-12-07 11:20:37 +0100

    Update main.tf

--- jeny_aws/main.tf
+++ jeny_aws/main.tf
@@ -19,6 +19,7 @@ resource "aws_instance" "cda_instance" {
 		usermod -aG docker ec2-user
 		curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose	
 		chmod +x /usr/local/bin/docker-compose
+		sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 		docker-compose --version
 		yum install -y git
 		mkdir  /tmp/jenya
