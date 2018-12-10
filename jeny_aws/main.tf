5cda01abff5a1e6f930d3556286472fb7a303e8e
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2018-12-10 15:02:39 +0100

    Update main.tf

--- jeny_aws/main.tf
+++ jeny_aws/main.tf
@@ -46,22 +46,22 @@ output "public_ip" {
 
 output "id" {
 	description = "List of IDs of instances"
-	value       = ["${aws_instance.cda_instance.*.id[0]}"]
+	value       = "${aws_instance.cda_instance.*.id[0]}"
 }
 
 output "availability_zone" {
 	description = "List of availability zones of instances"
-	value       = ["${aws_instance.cda_instance.*.availability_zone[0]}"]
+	value       = "${aws_instance.cda_instance.*.availability_zone[0]}"
 }
 
 output "key_name" {
 	description = "List of key names of instances"
-	value       = ["${aws_instance.cda_instance.*.key_name[0]}"]
+	value       = "${aws_instance.cda_instance.*.key_name[0]}"
 }
 
 output "public_dns" {
 	description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-	value       = ["${aws_instance.cda_instance.*.public_dns[0]}"]
+	value       = "${aws_instance.cda_instance.*.public_dns[0]}"
 }
 
 output "network_interface_id" {
@@ -71,30 +71,30 @@ output "network_interface_id" {
 
 output "primary_network_interface_id" {
 	description = "List of IDs of the primary network interface of instances"
-	value       = ["${aws_instance.cda_instance.*.primary_network_interface_id[0]}"]
+	value       = "${aws_instance.cda_instance.*.primary_network_interface_id[0]}"
 }
 
 output "private_dns" {
 	description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-	value       = ["${aws_instance.cda_instance.*.private_dns[0]}"]
+	value       = "${aws_instance.cda_instance.*.private_dns[0]}"
 }
 
 output "private_ip" {
 	description = "List of private IP addresses assigned to the instances"
-	value       = ["${aws_instance.cda_instance.*.private_ip[0]}"]
+	value       = "${aws_instance.cda_instance.*.private_ip[0]}"
 }
 
 output "security_groups" {
 	description = "List of associated security groups of instances"
-	value       = ["${aws_instance.cda_instance.*.security_groups[0]}"]
+	value       = "${aws_instance.cda_instance.*.security_groups[0]}"
 }
 
 output "vpc_security_group_ids" {
 	description = "List of associated security groups of instances, if running in non-default VPC"
-	value       = ["${aws_instance.cda_instance.*.vpc_security_group_ids[0]}"]
+	value       = "${aws_instance.cda_instance.*.vpc_security_group_ids[0]}"
 }
 
 output "subnet_id" {
 	description = "List of IDs of VPC subnets of instances"
-	value       = ["${aws_instance.cda_instance.*.subnet_id[0]}"]
+	value       = "${aws_instance.cda_instance.*.subnet_id[0]}"
 }
