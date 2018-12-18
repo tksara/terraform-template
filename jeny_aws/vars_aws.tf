95bda3b80f700433075d880cd84a41dc3fb51641
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2018-12-18 14:52:49 +0100

    Update vars_aws.tf

--- jeny_aws/vars_aws.tf
+++ jeny_aws/vars_aws.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-	default = "alabala"
+  default = "alabala"
 }
 
 variable "aws_secret_key" {
-	default = "alabala"
+  default = "alabala"
 }
 
 variable "aws_key_name" {
@@ -15,11 +15,7 @@ variable "private_key_file" {
 }
 variable "aws_ami" {
 	default = "ami-0080e4c5bc078760e"
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> 4e8dbad33ac2b09b9f792e35eb9cbaf26334cbb0
 }
 
 variable "aws_security_group_id" {
