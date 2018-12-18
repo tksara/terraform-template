fc30a4f350ff31f86675f0500835dc23d5cdbe3f
Jeny Stoeva <you@example.com>
2018-12-18 15:20:38 +0100

    adding new templates

---
+++ aws_basic/main.tf
@@ -0,0 +1,17 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
+variable "aws_security_group_id" {}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_instance" "cda_instance" {
+  ami                    = "${var.aws_ami}"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${var.aws_security_group_id}"]
+  key_name	= "jeny-key-us-east-1"
+}
---
+++ gc_basic/main.tf
@@ -0,0 +1,30 @@
+variable "project" {default = "my_project_id"}
+
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = "${var.project}"
+  region      = "us-central1"
+}
+
+data "google_compute_zones" "available" {}
+
+resource "google_compute_instance" "default" {
+  project = "${var.project}"
+  zone = "${data.google_compute_zones.available.names[0]}"
+  name = "tf-compute-1"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20170328"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+output "instance_id" {
+  value = "${google_compute_instance.default.self_link}"
+}
