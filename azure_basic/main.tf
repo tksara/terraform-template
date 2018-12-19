1874c8393c3d985fc0ffca9230c00409da9159da
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2018-12-19 15:49:47 +0100

    Update main.tf

--- azure_basic/main.tf
+++ azure_basic/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "test" {
 	name = "test_group"
-	location = "EastUS"
+	location = "eastus"
 }
 
 resource "azurerm_virtual_network" "test_instance" {
