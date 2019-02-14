64c42c74458ada1ca1a60203e6fdf8b4ec7857e5
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-14 15:46:16 +0100

    Update main.tf

--- gc_basic/main.tf
+++ gc_basic/main.tf
@@ -13,7 +13,7 @@ resource "google_compute_instance" "default" {
   project = "${var.project}"
   zone = "${data.google_compute_zones.available.names[0]}"
   name = "tf-compute-1"
-  machine_type = "f1-micro"
+  machine_type = "Jeny-EM Test Instance"
   boot_disk {
     initialize_params {
       image = "ubuntu-1604-xenial-v20190212"
