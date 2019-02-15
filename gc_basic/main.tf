9c6f86fd8faa249d902d2f82a2a9fc0c9f5ef5e8
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-15 13:39:14 +0100

    Update main.tf

--- gc_basic/main.tf
+++ gc_basic/main.tf
@@ -32,6 +32,11 @@ provider "google" {
   region      = "us-west1"
 }
 
+resource "google_compute_network" "default" {
+  name                    = "${var.network_name}"
+  auto_create_subnetworks = false
+}
+
 resource "google_compute_subnetwork" "default" {
   name                     = "${var.network_name}"
   ip_cidr_range            = "${var.network_cidr}"
