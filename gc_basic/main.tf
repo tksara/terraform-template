0d4e21f1d0d3ccb65cd96b5d3f0ddc0340041f92
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-19 11:28:42 +0100

    Update main.tf

--- gc_basic/main.tf
+++ gc_basic/main.tf
@@ -26,7 +26,7 @@ resource "google_compute_instance" "default" {
   count        = "${var.num_nodes}"
   project      = "${var.project}"
   zone         = "us-west1-b"
-  name         = "jeny-em-test-${count.index + 1}"
+  name         = "jeny-em-test-${count.index + 1}-${local.in}"
   machine_type = "f1-micro"
   
   boot_disk {
