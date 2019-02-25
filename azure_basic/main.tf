24dce7e59cf0186b4ab6791599c0a5fe04ef0bf7
Jeny Stoeva <you@example.com>
2019-02-25 16:03:59 +0100

    vmware folder upload

--- azure_basic/main.tf
+++ azure_basic/main.tf
@@ -2,59 +2,161 @@ variable "client_secret" {}
 variable "subscription_id" {}
 variable "client_id" {}
 variable "tenant_id" {}
+variable "key_data" {}
 
 provider "azurerm" {
-	version = "1.20.0"
 
 	subscription_id = "${var.subscription_id}"
 	client_id       = "${var.client_id}"
 	client_secret   = "${var.client_secret}"
 	tenant_id       = "${var.tenant_id}"
+	key_data        = "${var.key_data}"
 }
 
-resource "azurerm_resource_group" "test" {
-	name = "test_group"
-	location = "East US"
-}
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "myterraformgroup" {
+	name     = "myResourceGroup"
+	location = "eastus"
 
-resource "azurerm_virtual_network" "test_instance" {
-	address_space = ["10.0.0.0/16"]
-	location = "${azurerm_resource_group.test.location}}"
-	name = "test_instance"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-}
-
-resource "azurerm_subnet" "internal" {
-	name                 = "internal"
-	resource_group_name  = "${azurerm_resource_group.test.name}"
-	virtual_network_name = "${azurerm_virtual_network.test_instance.name}"
-	address_prefix       = "10.0.2.0/24"
-}
-
-resource "azurerm_network_interface" "test_instance" {
-	name                = "network_interface"
-	location            = "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-
-	ip_configuration {
-		name                          = "testconfiguration1"
-		subnet_id                     = "${azurerm_subnet.internal.id}"
-		private_ip_address_allocation = "dynamic"
+	tags {
+		environment = "Terraform Demo"
 	}
 }
 
-resource "azurerm_virtual_machine" "main" {
-	name = "test-vm"
-	location = "${azurerm_resource_group.test.location}"
-	resource_group_name = "${azurerm_resource_group.test.name}"
-	network_interface_ids = [
-		"${azurerm_network_interface.test_instance.id}"]
-	vm_size = "Standard_DS1_v2"
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+	name                = "myVnet"
+	address_space       = ["10.0.0.0/16"]
+	location            = "eastus"
+	resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+	tags {
+		environment = "Terraform Demo"
+	}
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+	name                 = "mySubnet"
+	resource_group_name  = "${azurerm_resource_group.myterraformgroup.name}"
+	virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+	address_prefix       = "10.0.1.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+	name                         = "myPublicIP"
+	location                     = "eastus"
+	resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
+	public_ip_address_allocation = "dynamic"
+
+	tags {
+		environment = "Terraform Demo"
+	}
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+	name                = "myNetworkSecurityGroup"
+	location            = "eastus"
+	resource_group_name = "${azurerm_resource_group.myterraformgroup.name}"
+
+	security_rule {
+		name                       = "SSH"
+		priority                   = 1001
+		direction                  = "Inbound"
+		access                     = "Allow"
+		protocol                   = "Tcp"
+		source_port_range          = "*"
+		destination_port_range     = "22"
+		source_address_prefix      = "*"
+		destination_address_prefix = "*"
+	}
+
+	tags {
+		environment = "Terraform Demo"
+	}
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+	name                      = "myNIC"
+	location                  = "eastus"
+	resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
+	network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+
+	ip_configuration {
+		name                          = "myNicConfiguration"
+		subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+		private_ip_address_allocation = "dynamic"
+		public_ip_address_id          = "${azurerm_public_ip.myterraformpublicip.id}"
+	}
+
+	tags {
+		environment = "Terraform Demo"
+	}
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+	keepers = {
+		# Generate a new ID only when a new resource group is defined
+		resource_group = "${azurerm_resource_group.myterraformgroup.name}"
+	}
+
+	byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+	name                        = "diag${random_id.randomId.hex}"
+	resource_group_name         = "${azurerm_resource_group.myterraformgroup.name}"
+	location                    = "eastus"
+	account_tier                = "Standard"
+	account_replication_type    = "LRS"
+
+	tags {
+		environment = "Terraform Demo"
+	}
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+	name                  = "myVM"
+	location              = "eastus"
+	resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+	network_interface_ids = ["${azurerm_network_interface.myterraformnic.id}"]
+	vm_size               = "Standard_DS1_v2"
 
 	storage_os_disk {
-		name              = "myosdisk1"
+		name              = "myOsDisk"
 		caching           = "ReadWrite"
 		create_option     = "FromImage"
-		managed_disk_type = "Standard_LRS"
+		managed_disk_type = "Premium_LRS"
+	}
+
+	storage_image_reference {
+		publisher = "Canonical"
+		offer     = "UbuntuServer"
+		sku       = "16.04.0-LTS"
+		version   = "latest"
+	}
+
+	os_profile {
+		computer_name  = "myvm"
+		admin_username = "azureuser"
+	}
+
+	os_profile_linux_config {
+		disable_password_authentication = true
+	}
+
+	boot_diagnostics {
+		enabled = "true"
+		storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+	}
+
+	tags {
+		environment = "Terraform Demo"
 	}
 }
