ba617d9a39daba01edabc5e6a6c6ff77a7a3babb
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-26 11:24:57 +0100

    Update main.tf

--- POC_template/main.tf
+++ POC_template/main.tf
@@ -1,20 +1,54 @@
-provider "aws" {
-	region     = "ap-southeast-1"
-	access_key = "${var.aws_access}"
-	secret_key = "${var.aws_secret}"
+variable "vsphere_user" {default =""}
+variable "vsphere_password" {default =""}
+variable "vsphere_server" {default = "vvievc01.sbb01.spoc.global"}
+
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
 }
 
-resource "aws_instance" "nguta04_sample" {
-	ami                    = "ami-6378f600"
-	instance_type          = "t2.micro"
-//	vpc_security_group_ids = ["sg-03c984013c3969e2a"]
-	key_name               = "nguta04"
+data "vsphere_datacenter" "dc" {
+  name = "Test"
+}
 
-	tags {
-		Name = "nguta04_machine"
-	}
+data "vsphere_datastore" "datastore" {
+  name          = "extvimDatastorcluster/local Storage svievmw04"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "normalPerf"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "nguta04_sample" {
+  name             = "nguta04_machine"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
 	
-	provisioner "remote-exec" {
+  provisioner "remote-exec" {
 		inline = [
 			"mkdir -p /home/ubuntu/AE"
 		]
