e12aa2ad54ed4e9d159cc038d3ee66475ac3e7f5
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-26 15:25:41 +0100

    Update main.tf

--- vmware_basic/main.tf
+++ vmware_basic/main.tf
@@ -61,10 +61,6 @@ resource "vsphere_virtual_machine" "vm" {
         host_name = "terraform-test"
         domain    = "test.internal"
       }
-      
-      network_interface {
-        network_id   = "${data.vsphere_network.public.id}"
-      }
     }  
   }
 }
