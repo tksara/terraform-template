5721f00e13e1f2573643b83ccef3e97dac80e035
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-28 15:50:21 +0100

    Update vars_agent_sm.tf

--- create_instance_install_agent/vars_agent_sm.tf
+++ create_instance_install_agent/vars_agent_sm.tf
@@ -10,9 +10,8 @@ variable "agent_pass" {
 	default = "ubuntu"
 }
 
-
 variable "ae_host" {
-	default = "172.31.43.107" # Private IP of Proxy Server
+	default = "10.243.44.139" # Private IP of Proxy Server
 }
 
 variable "ae_port" {
