d0e6e84a846a85078b33f0edd330ba04d728d700
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-02-28 13:19:09 +0100

    Update main.tf

--- vmware_basic/main.tf
+++ vmware_basic/main.tf
@@ -31,7 +31,7 @@ data "vsphere_network" "network" {
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "ubuntu-template"
+  name          = "ubuntu-minimal-template"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
