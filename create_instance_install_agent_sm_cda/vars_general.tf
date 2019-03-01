3efff79e1c79d9a3c5d6861fe8addc3e01857b1b
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-03-01 13:32:33 +0100

    Update vars_general.tf

--- create_instance_install_agent_sm_cda/vars_general.tf
+++ create_instance_install_agent_sm_cda/vars_general.tf
@@ -1,7 +1,15 @@
 variable "remote_working_dir" {
-	default = "/home/ubuntu/AE"
+	default = "/home/automic/AE"
 }
 
 variable "local_scripts_location" {
 	default = "/home/lefdo01/TNV/terraform-provisioning/scripts/local"
+}
+
+variable "private_key_file" {
+	default = "./id_rsa"
+}
+
+variable "ubuntu_password" {
+	default = ""
 }
