7b6dacfcd9845d45caf2b43039e1029e6c5ab46d
Jeny Stoeva <you@example.com>
2019-03-26 11:29:42 +0100

    adding gc template with service now integration

---
+++ gc_basic_with_servicenow_integration/main.tf
@@ -0,0 +1,42 @@
+variable "project" {default = "esd-general-dev"}
+
+variable "credentials" {}
+
+variable "num_nodes" {
+  description = "Number of nodes to create"
+  default     = 2
+}
+
+locals {
+	id = "${random_integer.name_extension.result}"
+}
+
+resource "random_integer" "name_extension" {
+  min     = 1
+  max     = 99999
+}
+
+provider "google" {
+  credentials = "${var.credentials}"
+  project     = "${var.project}"
+  region      = "us-west1"
+}
+
+resource "google_compute_instance" "default" {
+  count        = "${var.num_nodes}"
+  project      = "${var.project}"
+  zone         = "us-west1-b"
+  name         = "jeny-em-test-${count.index + 1}-${local.id}"
+  machine_type = "f1-micro"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-xenial-v20190212"
+    }
+  }
+  
+  network_interface {
+    subnetwork = "test-network-sub"
+    subnetwork_project ="esd-general-dev"
+  }
+}
