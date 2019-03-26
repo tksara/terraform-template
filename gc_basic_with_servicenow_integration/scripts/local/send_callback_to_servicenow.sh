6e2bc725c83a88d6a540efa3b2795a20d08a441c
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-03-26 15:15:04 +0100

    Update send_callback_to_servicenow.sh

--- gc_basic_with_servicenow_integration/scripts/local/send_callback_to_servicenow.sh
+++ gc_basic_with_servicenow_integration/scripts/local/send_callback_to_servicenow.sh
@@ -1,46 +1,12 @@
 #!/bin/bash
 
-CDA_HOST=$1
-CDA_USER=$2
-CDA_PASS=$3
-
-AGENT_NAME=$4
-DEPLTARGET=$5
-
-TOMCAT_HOST=$6
-TOMCAT_HOME_DIR=$7
-TOMCAT_USER=$8
-TOMCAT_PASS=$9
-
-FOLDER=${10}
-
-AUTH=$(echo -n "$CDA_USER:$CDA_PASS" | base64)
+IP=$1
 
 BODY=$(cat << EOF
 {
-	"status": "Active",
-	"agent": "$AGENT_NAME",
-	"name": "$DEPLTARGET",
-	"custom_type": {
-		"name": "Tomcat"
-	},
-	"custom": {
-		"appbase_directory": "webapps",
-		"home_directory": "$TOMCAT_HOME_DIR",
-		"protocol": "http",
-		"host": "$TOMCAT_HOST",
-		"username": "$TOMCAT_USER",
-		"password": "$TOMCAT_PASS",
-		"port": 8080
-	},
-	"folder": {
-		"name": "$FOLDER"
-	},
-	"owner": {
-		"name": "$CDA_USER"
-	}
+	"instanceIp": "$IP"
 }
 EOF
 )
 
-curl -X POST $CDA_HOST/api/data/v1/deployment_targets -H "Authorization: Basic $AUTH" -H 'Content-Type: application/json' -d "$BODY"
+curl -X POST https://ven01183.service-now.com/servicenowCallbackUrl.do -H 'Content-Type: application/json' -d "$BODY"
