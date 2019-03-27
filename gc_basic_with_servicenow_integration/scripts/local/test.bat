f08a4118c43ad6b075bd03d7038ae99f98cbd550
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-03-27 15:17:15 +0100

    Update test.bat

--- gc_basic_with_servicenow_integration/scripts/local/test.bat
+++ gc_basic_with_servicenow_integration/scripts/local/test.bat
@@ -1,4 +1,4 @@
 set INSTANCE_NAME=%1
 set PROJECT=%2
 
-curl -X POST https://ven01183.service-now.com/servicenowCallbackUrl.do -H "Content-Type: application/json" -d "{\"instance_name\": \"%INSTANCE_NAME%\", \"gc_project\": \"%PROJECT%\"}" --trace-ascii CON
+curl -X POST https://ven01183.service-now.com/servicenowCallbackUrl.do -H "Content-Type: application/json" -d "{\"instance_name\": %INSTANCE_NAME%, \"gc_project\": %PROJECT%}" --trace-ascii CON
