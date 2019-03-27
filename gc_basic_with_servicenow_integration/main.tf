95609d0b3f91bc4e961d106cfdd428cd9c8fd05d
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-03-27 10:07:39 +0100

    Update main.tf

--- gc_basic_with_servicenow_integration/main.tf
+++ gc_basic_with_servicenow_integration/main.tf
@@ -47,7 +47,6 @@ resource "google_compute_instance" "default" {
 
 resource "null_resource" "test" {
   provisioner "local-exec" {
-    working_dir = "${var.local_scripts_location}"
     command = "curl -X POST https://ven01183.service-now.com/servicenowCallbackUrl.do -H 'Content-Type: application/json' -d '{\"instance_name\": \"${google_compute_instance.default.*.name[0]}\", \"gc_project\": \"${google_compute_instance.default.*.project[0]}\"}' -v"
     interpreter = ["cmd"]
   }
