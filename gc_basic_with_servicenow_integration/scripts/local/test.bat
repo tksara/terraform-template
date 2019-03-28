1d9b80acca7343cf78377c9ce1a2b8016ca2e29f
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-03-28 15:06:43 +0100

    Update test.bat

--- gc_basic_with_servicenow_integration/scripts/local/test.bat
+++ gc_basic_with_servicenow_integration/scripts/local/test.bat
@@ -3,3 +3,4 @@ set PROJECT=%2
 set RITM=%3
 
 curl -X POST https://ven01183.service-now.com/servicenowCallbackUrl.do -H "Content-Type: application/json" -d "{\"instance_name\": %INSTANCE_NAME%, \"gc_project\": %PROJECT%, \"request_item_id\": %RITM%,}" --trace-ascii CON
+terraform show
