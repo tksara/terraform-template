fabba5dd7cd92c0771fcb1b635e7ca16ab41283c
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-04-11 15:25:16 +0200

    Update servicenow.bat

--- sn_vmware_instance_plus_agent_sm_cda_entities/scripts/local/servicenow.bat
+++ sn_vmware_instance_plus_agent_sm_cda_entities/scripts/local/servicenow.bat
@@ -1,11 +1,5 @@
-
-set INSTANCE_NAME=%1
-set PROJECT=%2
-set RITM=%3
-
 set SYS_ID=%1
 set USER_TOKEN=%2
-set INSTANCE_NAME=%3
-set INSTANCE_IP=%4
+set INSTANCE_IP=%3
 
-curl -X POST https://ven01183.service-now.com/api/now/table/sc_req_item/%SYS_ID% -H "Accept: application/json" -H "Content-Type: application/json" -H "X-UserToken: %USER_TOKEN%" -d "{\"comments\": %INSTANCE_NAME%, \"instance_ip\": %INSTANCE_IP%, \"request_item_id\": %RITM%}" --trace-ascii CON
+curl -X POST https://ven01183.service-now.com/api/now/table/sc_req_item/%SYS_ID% -H "Accept: application/json" -H "Content-Type: application/json" -H "X-UserToken: %USER_TOKEN%" -d "{\"comments\": %INSTANCE_IP%}" --trace-ascii CON
