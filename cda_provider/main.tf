5bcfedb44f05d53672ddc7c49815555ac20eccfa
Jeny Stoeva <you@example.com>
2019-07-04 14:33:29 +0200

    adding provider cda

---
+++ cda_provider/main.tf
@@ -0,0 +1,17 @@
+provider "cda" {
+  cda_server     = "${var.cda_server}"
+  user          = "${var.cda_user}"
+  password     = "${var.cda_password}"  
+}
+ 
+resource "cda_environment" "firstEnvironment" {
+  name  = "environment_name"
+  folder    = "DEFAULT"
+  custom_type  = "Generic"
+  dynamic = {}
+  custom = {}
+  deployment_targets = []
+  description = "Description Update"
+  owner = "100/AUTOMIC/AUTOMIC"
+  archived = true
+}
