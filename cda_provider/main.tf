742b572c8d6e2634f6405c2ec2bfee4d5acea7d9
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-07-04 14:53:39 +0200

    Update main.tf

--- cda_provider/main.tf
+++ cda_provider/main.tf
@@ -1,17 +1,21 @@
+variable "cda_server" {default = ""}
+variable "cda_user" {default = ""}
+variable "cda_password" {default = ""}
+
 provider "cda" {
   cda_server     = "${var.cda_server}"
-  user          = "${var.cda_user}"
-  password     = "${var.cda_password}"  
+  user           = "${var.cda_user}"
+  password       = "${var.cda_password}"  
 }
  
 resource "cda_environment" "firstEnvironment" {
-  name  = "environment_name"
-  folder    = "DEFAULT"
-  custom_type  = "Generic"
-  dynamic = {}
-  custom = {}
+  name               = "environment_name"
+  folder             = "DEFAULT"
+  custom_type        = "Generic"
+  dynamic            = {}
+  custom             = {}
   deployment_targets = []
-  description = "Description Update"
-  owner = "100/AUTOMIC/AUTOMIC"
-  archived = true
+  description        = "Description Update"
+  owner              = "100/AUTOMIC/AUTOMIC"
+  archived           = true
 }
