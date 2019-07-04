5e3c82b3b20e88bd885eb769d2efae2bb56b28bc
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-07-04 16:35:49 +0200

    Update main.tf

--- cda_provider/main.tf
+++ cda_provider/main.tf
@@ -9,7 +9,7 @@ provider "cda" {
 }
  
 resource "cda_environment" "firstEnvironment" {
-  name               = "jeny_test_1"
+  name               = "jeny_test_2"
   folder             = "DEFAULT"
   custom_type        = "Generic"
   description        = "Description Update"
