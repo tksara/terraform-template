22755a3c67fc198e68f7f47256fb7d6b41f03745
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-08-12 14:46:20 +0200

    Update main.tf

--- cda_provider/main.tf
+++ cda_provider/main.tf
@@ -1,4 +1,4 @@
-variable "cda_server" {default = "http://10.55.11.196/cda"}
+variable "cda_server" {default = "http://STOZH01L7480:80/CDA"}
 variable "cda_user" {default = "100/AUTOMIC/AUTOMIC"}
 variable "cda_password" {default = ""}
 
