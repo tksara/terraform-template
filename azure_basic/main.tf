ac4889292e38f3b10ef2ed6f611e2e93f6b17cd0
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-09-18 14:31:36 +0200

    Update main.tf

--- azure_basic/main.tf
+++ azure_basic/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
 	name                         = "myPublicIP"
 	location                     = "eastus"
 	resource_group_name          = "${azurerm_resource_group.myterraformgroup.name}"
-	public_ip_address_allocation = "dynamic"
+//	public_ip_address_allocation = "dynamic"
 /*
 	tags {
 		environment = "Terraform Demo"
