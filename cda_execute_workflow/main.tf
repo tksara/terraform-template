dfa347b5bdb0cd4d475e4ee130f4e4baad621e32
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-09-22 22:04:53 +0200

    Update main.tf

--- cda_execute_workflow/main.tf
+++ cda_execute_workflow/main.tf
@@ -88,7 +88,7 @@ resource "cda_workflow_execution" "my_execution" {
    overrides_component = [
 	{
 	   component_name = "webapp"
-	   "tomcat/test_property" = "kukurigu1"
+	   "tomcat/test_property" = "kukurigu2"
 	}
    ]	
 }
