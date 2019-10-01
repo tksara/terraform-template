bc8b7b02ad38274907cd8dff905e35a24a30deea
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-10-01 09:59:36 +0200

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -1,13 +1,13 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
  // access_key = "my-access-key"
  // secret_key = "my-secret-key"
 }
