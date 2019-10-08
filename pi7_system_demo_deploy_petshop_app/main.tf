de41e21dc6c0d633341cb919f4b366e5651d1963
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-10-08 14:15:44 +0200

    Update main.tf

--- pi7_system_demo_deploy_petshop_app/main.tf
+++ pi7_system_demo_deploy_petshop_app/main.tf
@@ -1,38 +1,21 @@
-//variable "cda_server" {default = "http://10.55.19.254/cda"}
 variable "cda_server" {default = "http://STOZH01L7480/cda"}
-//variable "cda_server" {default = "http://STOZH01L7480:80/cda"}
 variable "cda_user" {default = "100/AUTOMIC/AUTOMIC"}
 variable "cda_password" {default = ""}
-variable "component_name" {default = "Component A"}
-
-locals {
-	id = "${random_integer.name_extension.result}"
-}
-
-resource "random_integer" "name_extension" {
-  min     = 1
-  max     = 99999
-}
 
 provider "cda" {
   cda_server     = "${var.cda_server}"
   user           = "${var.cda_user}"
   password       = "${var.cda_password}"  
+	
+  default_attributes = { // default_attributes can be used to set the 'folder' and 'owner' attributes globally for the template.
+    folder = "DEFAULT"
+    owner  = "100/AUTOMIC/AUTOMIC"
+  }
 }
  
-resource "cda_environment" "firstEnvironment" {
-  name               = "jeny-${local.id}"
+resource "cda_environment" "demoEnvironment" {
+  name               = "Pet Shop PROD"
   folder             = "DEFAULT"
-  type               = "Generic"
-  description        = "Description Update"
-  owner              = "100/AUTOMIC/AUTOMIC"
-  
-  dynamic_properties = {
-      "name1" = "value1"
-      "name2" = "value2"
-  }
-  
-  custom_properties = {}
   
   deployment_targets = ["${cda_deployment_target.jenys_target.name}", "Local Tomcat"]
 }
@@ -40,9 +23,7 @@ resource "cda_environment" "firstEnvironment" {
 resource "cda_deployment_target" "jenys_target" {
   name        = "jeny-${local.id}"
   type        = "Database JDBC"
-  folder      = "DEFAULT"
-  owner       = "100/AUTOMIC/AUTOMIC"
-  //agent       = "WIN01"
+  agent       = "WIN01" //TODO Install the Agent
 }
 
 resource "cda_login_object" "my_login_object" {
@@ -62,8 +43,6 @@ resource "cda_login_object" "my_login_object" {
 
 resource "cda_deployment_profile" "my_deployment_profile" {
   name         = "test_profile-${local.id}"
-  folder       = "DEFAULT"
-  owner        = "100/AUTOMIC/AUTOMIC"
   application  = "IM Test App"
   environment  = "${cda_environment.firstEnvironment.name}"
   login_object = "${cda_login_object.my_login_object.name}"
@@ -75,13 +54,10 @@ resource "cda_deployment_profile" "my_deployment_profile" {
 }
 
 resource "cda_workflow_execution" "my_execution" {
-  enable = "true"
+  triggers                     = "true"
   application                  = "application" 
   workflow                     = "workflow name" 
   package                      = "package" 
   deployment_profile           = "my_deployment_profile" 
-  //manual_approval              = "true" 
-  //approver                     = "100/AUTOMIC/AUTOMIC"
-  //schedule                     = "2019-12-28T13:44:00Z" //"cron(0 3 12 12 ? 2019)" //start_date = "2019-12-12" //start_time = "3:00"  
   override_existing_components = "false"
 }
