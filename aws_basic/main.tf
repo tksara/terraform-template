d41c77da310d00c335050932982fb2dfae5b126f
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-10-10 12:42:15 +0200

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -1,6 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
+//variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
+variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
 
@@ -8,13 +9,11 @@ provider "aws" {
   region     = "us-east-1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
- // access_key = "my-access-key"
- // secret_key = "my-secret-key"
 }
 
 resource "aws_instance" "cda_instance" {
   ami                    = "${var.aws_ami}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${var.aws_security_group_id}"]
-  //key_name	= "jeny-key-us-east-1"
+  key_name	             = "jeny-key-us-east-1"
 }
