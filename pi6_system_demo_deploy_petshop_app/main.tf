997ca55b0f878710db84e2f7e48f03d1026c921b
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-10-10 10:33:47 +0200

    Update main.tf

--- pi6_system_demo_deploy_petshop_app/main.tf
+++ pi6_system_demo_deploy_petshop_app/main.tf
@@ -1,7 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 //variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
-variable "aws_ami" {default = "ami-0bba96c31d87e65d9"}
+variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
 variable "cda_server" {default = "http://STOZH01L7480/cda"}
@@ -29,8 +29,8 @@ resource "aws_instance" "cda_instance" {
 	
   user_data = <<HEREDOC
 		#!/bin/bash -v
-                sudo yum update -y
-                sudo yum install -y tomcat7-webapps tomcat7-docs-webapp tomcat7-admin-webapps
+                sudo apt update -y
+                sudo apt install -y tomcat7-webapps tomcat7-docs-webapp tomcat7-admin-webapps
                 sudo service tomcat7 start 
   HEREDOC
   
