28c18dcf2f2a929b1481a8533b9285d62627dc78
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-11-06 16:38:37 +0100

    Update main.tf

--- pi6_system_demo_deploy_petshop_app/main.tf
+++ pi6_system_demo_deploy_petshop_app/main.tf
@@ -124,7 +124,7 @@ provider "cda" {
 	
   default_attributes = { // default_attributes can be used to set the 'folder' and 'owner' attributes globally for the template.
     folder = "DEFAULT"
-    owner  = "100/BOND/AUTOMIC"
+    owner  = "100/AUTOMIC/AUTOMIC"
   }
 }
  
