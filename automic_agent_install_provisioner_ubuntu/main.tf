650b08374e60772eddf2aa8f08ab17d4fdfb34af
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-11-07 15:33:29 +0100

    Update main.tf

--- automic_agent_install_provisioner_ubuntu/main.tf
+++ automic_agent_install_provisioner_ubuntu/main.tf
@@ -32,7 +32,6 @@ resource "aws_instance" "cda_instance" {
   		destination = "${var.remote_working_dir}"
     		source = "C:\\Terraform\\EM\\Jenys Templates\\Binaries"
 
-    		//agent_name_prefix = "${var.agent_name_prefix}"
     		agent_name = "${random_string.cda_entity_name.result}"
     		agent_port = "${var.agent_port}"
     		ae_system_name = "${var.ae_system_name}"
