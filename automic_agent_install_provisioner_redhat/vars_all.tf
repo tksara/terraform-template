64c749b1d8e81679a1f75f88e49df8735935f553
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-11-08 12:28:04 +0100

    Update vars_all.tf

--- automic_agent_install_provisioner_redhat/vars_all.tf
+++ automic_agent_install_provisioner_redhat/vars_all.tf
@@ -3,8 +3,11 @@ variable "aws_secret_key" {}
 variable "aws_ami" {default = "ami-0551d2acb56d6e88c"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
-variable "cda_server" {default = "http://STOZH01L7480/cda"}
-variable "cda_user" {default = "100/AUTOMIC/AUTOMIC"}
-variable "cda_password" {}
 variable "remote_working_dir" {default = "/home/ubuntu/AE"}
 variable "private_key_file" {default = "C:\\Terraform\\EM\\AWS_Key\\jeny-key-us-east-1.pem"}
+variable "agent_port" {default = "2300"}
+variable "ae_system_name" {default = "AUTOMIC"}
+variable "ae_host" {default = "3.88.27.73"}
+variable "ae_port" {default = "2217"}
+variable "sm_port" {default = "8871"}
+variable "sm_name" {default = "sm_"}
