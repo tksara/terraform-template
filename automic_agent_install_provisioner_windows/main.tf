12f27a39e839595f6bc5c55b2e52f6be547084c4
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-11-29 14:44:07 +0100

    Update main.tf

--- automic_agent_install_provisioner_windows/main.tf
+++ automic_agent_install_provisioner_windows/main.tf
@@ -8,15 +8,15 @@ resource "aws_instance" "cda_instance" {
   ami                    = "${var.aws_ami}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${var.aws_security_group_id}"]
-  key_name	         = "jeny-key-us-east-1"
+  key_name	             = "jeny-key-us-east-1"
 
   tags = {
-    Name = "agent_provisioner_instance"
+      Name = "agent_provisioner_instance"
   }
   
   user_data     = <<EOF
   <powershell>
-    net user ${var.agent_user} '${var.agent_pass}'
+    net user ${var.aws_instance_user} '${var.aws_instance_pass}'
     winrm quickconfig -q
     winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
     winrm set winrm/config '@{MaxTimeoutms="1800000"}'
@@ -31,28 +31,28 @@ resource "aws_instance" "cda_instance" {
   EOF
  /*
   provisioner "automic_agent_install" {
-    source = "C:\Terraform\EM\Binaries\windows"
-    destination = "${var.remote_working_dir}"
+    source         = "C:\Terraform\EM\Binaries\windows"
+    destination    = "${var.remote_working_dir}"
 
-    agent_name = "${random_string.cda_entity_name.result}"
-    agent_port = "${var.agent_port}"
+    agent_name     = "${random_string.cda_entity_name.result}"
+    agent_port     = "${var.agent_port}"
     ae_system_name = "${var.ae_system_name}"
-    ae_host = "${var.ae_host}"
-    ae_port = "${var.ae_port}"
-    sm_port = "${var.sm_port}"
-    sm_name = "${var.sm_name}${random_string.cda_entity_name.result}"
+    ae_host        = "${var.ae_host}"
+    ae_port        = "${var.ae_port}"
+    sm_port        = "${var.sm_port}"
+    sm_name        = "${var.sm_name}${random_string.cda_entity_name.result}"
 
     variables = {
       UC_EX_PATH_JOBREPORT = "../tmp"
-      UC_EX_PATH_TEMP = "../tmp"
-      UC_EX_IP_ADDR = "${self.public_ip}"
+      UC_EX_PATH_TEMP      = "../tmp"
+      UC_EX_IP_ADDR        = "${self.public_ip}"
     }
 
     connection {
-      host = self.public_ip
+      host        = self.public_ip
       type        = "winrm"
-      user        = "${var.agent_user}"
-      password = "${var.agent_pass}"
+      user        = "${var.aws_instance_user}"
+      password    = "${var.aws_instance_pass}"
     }
   }  */
 }
