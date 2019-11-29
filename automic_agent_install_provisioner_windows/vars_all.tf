c127935bf120b901f1ddb4597c76b7d6c4f2743f
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2019-11-29 13:48:16 +0100

    Update vars_all.tf

--- automic_agent_install_provisioner_windows/vars_all.tf
+++ automic_agent_install_provisioner_windows/vars_all.tf
@@ -1,6 +1,6 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
+variable "aws_ami" {default = "ami-00dd3069"}
 variable "aws_security_group_id" {default = "sg-495c840a"}
 variable "instance_type" {default = "t2.micro"}
 variable "remote_working_dir" {default = "/home/ubuntu/AE"}
