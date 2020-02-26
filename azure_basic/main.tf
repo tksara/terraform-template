4afd426f464603a91127dfef3c0aac5aec77751c
Jeny Stoeva <38282204+jenyss@users.noreply.github.com>
2020-02-26 14:14:26 +0100

    Update main.tf

--- azure_basic/main.tf
+++ azure_basic/main.tf
@@ -21,6 +21,7 @@ provider "azurerm" {
 	client_id       = "${var.azure_client_id}"
 	client_secret   = "${var.azure_client_secret}"
 	tenant_id       = "${var.azure_tenant_id}"
+	features {}
 }
 
 # Create a resource group if it doesn’t exist
