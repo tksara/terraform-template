c2e6f26c8a8b44082b3ed7d80f1bf9c99e18673d
tksara <son.dot@gmail.com>
2021-01-14 23:02:03 +0100

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -1,13 +1,13 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "access_key" {}
+variable "secret_key" {}
 variable "aws_ami" {default = "ami-03c3a7e4263fd998c"}
 variable "aws_security_group_id" {default = "sg-730e980c"}
 variable "instance_type" {default = "t2.micro"}
 
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "cda_instance" {
