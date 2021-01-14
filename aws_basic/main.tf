65b18f9c51b274a080d74ea8300c4d43b14dd187
tksara <son.dot@gmail.com>
2021-01-14 21:54:34 +0100

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -1,8 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-//variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
-variable "aws_ami" {default = "ami-04b9e92b5572fa0d1"}
-variable "aws_security_group_id" {default = "sg-495c840a"}
+variable "aws_ami" {default = "ami-03c3a7e4263fd998c"}
+variable "aws_security_group_id" {default = "sg-730e980c"}
 variable "instance_type" {default = "t2.micro"}
 
 provider "aws" {
@@ -15,5 +14,5 @@ resource "aws_instance" "cda_instance" {
   ami                    = "${var.aws_ami}"
   instance_type          = "${var.instance_type}"
   vpc_security_group_ids = ["${var.aws_security_group_id}"]
-  key_name	             = "jeny-key-us-east-1"
+  key_name	             = "im-aws"
 }
